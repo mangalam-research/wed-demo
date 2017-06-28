9b7e5edc3087b978bd7d30a876164e0d87cac6a4
Louis-Dominique Dubeau <ldd@lddubeau.com>
2017-06-28 14:52:43 -0400

    Add an alert method to the confirm service.

--- src/dashboard/confirm.service.ts
+++ src/dashboard/confirm.service.ts
@@ -5,30 +5,42 @@ import { safeConfirm } from "./util";
 
 export type Confirmer = (message: string) => Promise<boolean>;
 export type Prompter = (message: string) => Promise<string>;
+export type Alerter = (message: string) => Promise<void>;
 
-export function bootboxAdapter(message: string): Promise<string> {
+export function bootboxPrompter(message: string): Promise<string> {
   return new Promise((resolve) => {
     bootbox.prompt(message, resolve);
+  });
+}
+
+export function bootboxAlerter(message: string): Promise<void> {
+  return new Promise<void>((resolve) => {
+    bootbox.alert(message, resolve);
   });
 }
 
 @Injectable()
 export class ConfirmService {
   private readonly confirmer: Confirmer = safeConfirm;
-  private readonly prompter: Prompter = bootboxAdapter;
+  private readonly prompter: Prompter = bootboxPrompter;
+  private readonly alerter: Alerter = bootboxAlerter;
 
   // We cannot use TypeScript's support for default parameter values because
   // @Optional passes ``null`` if no value has been specified, but TypeScript's
   // generated code only checks for ``undefined`` when deciding to check whether
   // to use the default value. (IOW, an argument of ``null`` means: don't use
   // the default value. Sigh...
-  constructor(@Optional() @Inject("Confirmer") confirmer: Confirmer,
-              @Optional() @Inject("Prompter") prompter: Prompter) {
+  constructor(@Optional() @Inject("Confirmer") confirmer: Confirmer | undefined,
+              @Optional() @Inject("Prompter") prompter: Prompter | undefined,
+              @Optional() @Inject("Alerter") alerter: Alerter | undefined) {
     if (confirmer != null) {
       this.confirmer = confirmer;
     }
     if (prompter != null) {
       this.prompter = prompter;
+    }
+    if (alerter != null) {
+      this.alerter = alerter;
     }
   }
 
@@ -38,5 +50,9 @@ export class ConfirmService {
 
   prompt(message: string): Promise<string> {
     return this.prompter(message);
+  }
+
+  alert(message: string): Promise<void> {
+    return this.alerter(message);
   }
 }
