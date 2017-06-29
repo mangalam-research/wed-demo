a5fb4923dba7b60570f1752a83ac419fee4d300c
Louis-Dominique Dubeau <ldd@lddubeau.com>
2017-06-29 15:11:41 -0400

    Simplified kitchen sink and require pack parameter.
    Removed old code that does not pertain to the demo. And now we need a pack
    parameter to indicate which pack to load. This allows removing logic from the
    kitchen sink.

--- web/kitchen-sink.js
+++ web/kitchen-sink.js
@@ -24,189 +24,119 @@ define(function f(require) {
 
   var uri = new URI();
   var query = uri.query(true);
-  var mode = query.mode;
-  var file = query.file;
-  var schema = query.schema;
   var localstorage = query.localstorage;
   var options_param = query.options;
   var nodemo = query.nodemo;
   var managementURL = query.management;
+  // We *could* get the pack from the XMLFile object. However, when automatic
+  // association between packs and XML files is performed, this information is
+  // not readily available. Rather than duplicate the work that is done in the
+  // dashboard code proper, we require that the pack's link be passed as a
+  // parameter.
+  var packURL = query.pack;
 
-  if (file !== undefined && localstorage !== undefined) {
-    throw new Error("file and localstorage defined: use one or " +
-                    "the other");
+  if (localstorage === undefined) {
+    throw new Error("localstorage undefined");
   }
 
   Promise.try(function initial() {
-    if (localstorage) {
-      // Show the link...
-      var file_management_link = document.getElementById("fm-link");
-      file_management_link.style.display = "";
-      if (managementURL) {
-        file_management_link.href = managementURL;
-      }
-
-      return new Promise(function makePromise(resolve, reject) {
-        require(["wed-store"], resolve, reject);
-      }).then(function loaded(wedStore) {
-        var saverOptions = {
-          save: {
-            path: "wed/savers/indexeddb",
-            options: {
-              getStore: function getStore() {
-                return wedStore;
-              },
-              name: localstorage,
-            },
-          },
-        };
-
-        if (!localstorage.indexOf("://") === -1) {
-          return wedStore.get(localstorage).then(function then(value) {
-            return [value.data, undefined, saverOptions];
-          });
-        }
-
-        return [undefined, localstorage, saverOptions];
-      });
+    // Show the link...
+    var file_management_link = document.getElementById("fm-link");
+    file_management_link.style.display = "";
+    if (managementURL) {
+      file_management_link.href = managementURL;
     }
 
-    return [undefined, file];
-  }).spread(function launch(text, filePath, options) {
-    options = options || {};
-    if (text && filePath) {
-      throw new Error("text and file cannot be both defined");
-    }
-
-    if (mode) {
-      options.mode = { path: mode };
-    }
-
-    if (schema) {
-      switch (schema) {
-      case "@math":
-        options.schema = "/build/schemas/tei-math-rng.js";
-        options.mode = {
-          path: "wed/modes/generic/generic",
+    return new Promise(function makePromise(resolve, reject) {
+      require(["wed-store"], resolve, reject);
+    }).then(function loaded(wedStore) {
+      var options = {
+        save: {
+          path: "wed/savers/indexeddb",
           options: {
-            meta: {
-              path: "wed/modes/generic/metas/tei-meta",
-              options: {
-                metadata:
-                "/build/schemas/tei-math-metadata.json",
-              },
+            getStore: function getStore() {
+              return wedStore;
             },
+            name: localstorage,
           },
-        };
-        break;
-      case "@docbook":
-        options.schema = "/build/schemas/docbook.js";
-        options.mode = {
-          path: "wed/modes/generic/generic",
-          options: {
-            meta: {
-              path: "wed/modes/generic/metas/docbook-meta",
-              options: undefined,
-            },
-          },
-        };
-        break;
-      default:
-        options.schema = schema;
+        },
+      };
+
+      if (options_param === "noautoinsert") {
+        options.mode.options = { autoinsert: false };
       }
-    }
 
-    if (options_param === "noautoinsert") {
-      options.mode.options = { autoinsert: false };
-    }
-
-    if (options_param === "ambiguous_fileDesc_insert") {
-      options.mode.options = { ambiguous_fileDesc_insert: true };
-    }
-
-    if (options_param === "fileDesc_insert_needs_input") {
-      options.mode.options = { fileDesc_insert_needs_input: true };
-    }
-
-    if (options_param === "hide_attributes") {
-      options.mode.options = { hide_attributes: true };
-    }
-
-    // We don't want a demo dialog to show up in testing.
-    if (!nodemo) {
-      if (!localstorage) {
-        options.demo =
-          "You will not be able to save your modifications.";
+      if (options_param === "ambiguous_fileDesc_insert") {
+        options.mode.options = { ambiguous_fileDesc_insert: true };
       }
-      else {
-        options.demo =
-          "Your modifications will be saved in local storage.";
+
+      if (options_param === "fileDesc_insert_needs_input") {
+        options.mode.options = { fileDesc_insert_needs_input: true };
       }
-    }
 
-    var r = new runtime.Runtime(options);
+      if (options_param === "hide_attributes") {
+        options.mode.options = { hide_attributes: true };
+      }
 
-    var deps = [];
-    if (filePath) {
-      deps.push(filePath);
-    }
-
-    Promise.all(deps.map(r.resolve.bind(r)))
-      .then(function resolved(resolvedDeps) {
-        if (deps.length === 0) {
-          return undefined;
+      // We don't want a demo dialog to show up in testing.
+      if (!nodemo) {
+        if (!localstorage) {
+          options.demo =
+            "You will not be able to save your modifications.";
         }
-
-        var resolvedFile = resolvedDeps[0];
-        if (!resolvedFile) {
-          throw new Error("did not resolve file! " + deps[0]);
+        else {
+          options.demo =
+            "Your modifications will be saved in local storage.";
         }
+      }
 
-        if (typeof resolvedFile === "string") {
-          text = resolvedFile;
-          return undefined;
-        }
+      var r = new runtime.Runtime(options);
 
-        var packURL = store.db.makeIndexedDBURL(store.db.packs,
-                                                resolvedFile.pack);
-        // At this stage options.save.options.name contains a bogus value. Put
-        // the file name there.
-        options.save.options.name = resolvedFile.name;
-        return Promise.all([
-          store.db.chunks.get(resolvedFile.savedChunk)
-            .then(function loadedChunk(chunk) {
-              return chunk.getData();
-            })
-            .then(function resolvedData(data) {
-              // This is a file from the indexeddb system. Resolve the pack.
-              text = data;
+      var deps = [localstorage];
+
+      var text;
+      Promise.all(deps.map(r.resolve.bind(r)))
+        .then(function resolved(resolvedDeps) {
+          var resolvedFile = resolvedDeps[0];
+          // At this stage options.save.options.name contains a bogus value. Put
+          // the file name there.
+          options.save.options.name = resolvedFile.name;
+          return Promise.all([
+            store.db.chunks.get(resolvedFile.savedChunk)
+              .then(function loadedChunk(chunk) {
+                return chunk.getData();
+              })
+              .then(function resolvedData(data) {
+                // This is a file from the indexeddb system. Resolve the pack.
+                text = data;
+              }),
+            r.resolve(packURL).then(function packResolved(pack) {
+              var schemaURL = store.db.makeIndexedDBURL(store.db.chunks,
+                                                        pack.schema) + "/file";
+              var metadataURL = pack.metadata &&
+                  (store.db.makeIndexedDBURL(store.db.chunks, pack.metadata) +
+                   "/file");
+              options.schema = schemaURL;
+              options.mode = {
+                path: pack.mode,
+                options: {
+                },
+              };
+
+              var modeOptions = options.mode.options;
+              if (metadataURL) {
+                modeOptions.metadata = metadataURL;
+              }
             }),
-          r.resolve(packURL).then(function packResolved(pack) {
-            var schemaURL = store.db.makeIndexedDBURL(store.db.chunks,
-                                                      pack.schema) + "/file";
-            var metadataURL = pack.metadata &&
-                (store.db.makeIndexedDBURL(store.db.chunks, pack.metadata) +
-                 "/file");
-            options.schema = schemaURL;
-            options.mode = {
-              path: pack.mode,
-              options: {
-              },
-            };
-
-            var modeOptions = options.mode.options;
-            if (metadataURL) {
-              modeOptions.metadata = metadataURL;
-            }
-          }),
-        ]);
-      })
-      .then(function start() {
-        $(function ready() {
-          var widget = document.getElementById("widget");
-          window.wed_editor = new wed.Editor();
-          window.wed_editor.init(widget, options, text);
+          ]);
+        })
+        .then(function start() {
+          $(function ready() {
+            var widget = document.getElementById("widget");
+            window.wed_editor = new wed.Editor();
+            window.wed_editor.init(widget, options, text);
+          });
         });
-      });
+    });
   });
 });
