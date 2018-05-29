2096f5fad5dbf131a57bf14214f7361f1a4ae974
Louis-Dominique Dubeau <ldd@lddubeau.com>
2018-05-29 14:12:16 -0400

    build: refactor the gulp tasks.

--- gulptasks/util.js
+++ gulptasks/util.js
@@ -1,78 +1,10 @@
-const gulp = require("gulp");
-const gulpNewer = require("gulp-newer");
-const childProcess = require("child_process");
 const gutil = require("gulp-util");
 const fs = require("fs-extra");
 const del = require("del");
 const { execFile } = require("child-process-promise");
 
 exports.fs = fs;
-
-exports.mkdirp = fs.ensureDir;
 exports.del = del;
-
-exports.exec = function exec(command, options) {
-  return new Promise((resolve, reject) => {
-    childProcess.exec(command, options, (err, stdout, stderr) => {
-      if (err) {
-        gutil.log(stdout);
-        gutil.log(stderr);
-        reject(err);
-      }
-      resolve(stdout, stderr);
-    });
-  });
-};
-
-exports.newer = function newer(src, dest, forceDestFile) {
-  // We use gulp-newer to perform the test and convert it to a promise.
-  const options = {
-    dest,
-  };
-
-  if (forceDestFile) {
-    options.map = function map() {
-      return ".";
-    };
-  }
-
-  return new Promise((resolve) => {
-    const stream = gulp.src(src, { read: false })
-            .pipe(gulpNewer(options));
-
-    function end() {
-      resolve(false);
-    }
-
-    stream.on("data", () => {
-      stream.removeListener("end", end);
-      stream.end();
-      resolve(true);
-    });
-
-    stream.on("end", end);
-  });
-};
-
-exports.spawn = function spawn(cmd, args, options) {
-  return new Promise((resolve, reject) => {
-    const child = childProcess.spawn(cmd, args || [], options || {});
-
-    child.on("exit", (code, signal) => {
-      if (code) {
-        reject(new Error(`child terminated with code: ${code}`));
-        return;
-      }
-
-      if (signal) {
-        reject(new Error(`child terminated with signal: ${signal}`));
-        return;
-      }
-
-      resolve();
-    });
-  });
-};
 
 /**
  * Why use this over spawn with { stdio: "inherit" }? If you use this function,
